9c557bc7f4089fd4c5225bcb0fbbb9da695795ed
ekaterinagerasimova <evgerasimova13@gmail.com>
2017-12-05 13:20:11 +0400

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,16 @@
 package ru.job4j;
+
+/**
+ *Class Calculate создание первой программы
+ *@author egerasimova
+ *@since 26.11.2017
+*/
 	public class Calculate {
+
+/**
+ *Выводим на экран приветствие
+ *@return Результат, строка
+*/
 		public static void main(String[] args) {
 			System.out.println("Hello, World!");
  	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *Отдельный пакет для задания Calculate
+ *@author egerasimova (evgerasimova13@gmail.com)
+ *@version $Id$
+ *since 05.12.2017
+*/
+package ru.job4j;
