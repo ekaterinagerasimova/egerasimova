b9830bce51ac3e6b542d6af2c71577b9fd1a2bca
ekaterinagerasimova <evgerasimova13@gmail.com>
2018-04-09 14:32:14 +0400

    3.2. Конвертер валюты. [#41583]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Converter.java
@@ -0,0 +1,61 @@
+package ru.job4j.calculator;
+
+/**
+ * Корвертор валюты.
+ */
+public class Converter {
+    private int value;
+
+    /**
+     * Конвертируем рубли в евро.
+     * @param value рубли.
+     * @return Евро.
+     */
+    public int rubleToEuro(int value) {
+        int e = 70;
+        this.value = value / e;
+        return this.value;
+    }
+
+    /**
+     * Конвертируем рубли в доллары.
+     * @param value рубли.
+     * @return Доллоры
+     */
+    public int rubleToDollar(int value) {
+        int d = 60;
+        this.value = value / d;
+        return this.value;
+    }
+    /**
+     * Конвертируем доллоры в рубли.
+     * @param value доллоры.
+     * @return Рубли
+     */
+    public int dollarToRuble(int value) {
+        int d = 60;
+        this.value = value * d;
+        return  this.value;
+    }
+    /**
+     * Конвертируем евро в рубли.
+     * @param value евро.
+     * @return Рубли
+     */
+    public int euroToRuble(int value) {
+        int e = 70;
+        this.value = value * e;
+        return this.value;
+    }
+    public static void main(String[]args) {
+        Converter converter = new Converter();
+        int a = 180;
+        int b = 490;
+        int c = 10;
+
+        converter.rubleToEuro(b);
+        converter.rubleToDollar(a);
+        converter.euroToRuble(c);
+        converter.dollarToRuble(c);
+    }
+}
--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -21,7 +21,7 @@ public class CalculatorTest {
         assertThat(result, is(2D));
     }
     @Test
-    public void whenDiv2On2Then1(){
+    public void whenDiv2On2Then1() {
         Calculator calc1 = new Calculator();
         calc1.div(2D, 2D);
         double result = calc1.getResult();
@@ -29,7 +29,7 @@ public class CalculatorTest {
         assertThat(result, is(expected));
     }
     @Test
-    public void whenSubtract2Minus1Then1(){
+    public void whenSubtract2Minus1Then1() {
         Calculator calc2 = new Calculator();
         calc2.subtract(2D, 1D);
         double result = calc2.getResult();
@@ -37,7 +37,7 @@ public class CalculatorTest {
         assertThat(result, is(expected));
     }
     @Test
-    public void whenMiltiple1On1Then1(){
+    public void whenMiltiple1On1Then1() {
         Calculator calc3 = new Calculator();
         calc3.multiple(1D, 1D);
         double result = calc3.getResult();
